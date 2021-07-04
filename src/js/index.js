b6dcb5b2885279bf326dd56cee176530d39d5fbe
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2021-07-05 00:50:02 +0200

    add proper classes to the html file and implement sticky headerusing intersectionObserver API

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,8 @@ const navigationList = document.querySelector('.nav__links--js');
 const btnCloseModal = document.querySelector('.timetable__btn--js');
 const btnOpenModal = document.querySelector('.btn--show-modal--js');
 const timeTableContainer = document.querySelector('.timetable--js');
+const header = document.querySelector('.header--js');
+const sectionHome = document.querySelector('.section--home--js');
 
 navigationList.addEventListener('click', e => {
     e.preventDefault();
@@ -25,3 +27,20 @@ btnOpenModal.addEventListener('click', () => {
 btnCloseModal.addEventListener('click', () => {
     timeTableContainer.classList.add('hidden');
 });
+
+const headerSticky = entries => {
+    const [entry] = entries;
+    if (!entry.isIntersecting) {
+        header.classList.add('sticky');
+    } else {
+        header.classList.remove('sticky');
+    }
+};
+
+const sectionOptions = {
+    root: null,
+    threshold: 0.1,
+};
+
+const sectionObserver = new IntersectionObserver(headerSticky, sectionOptions);
+sectionObserver.observe(sectionHome);
