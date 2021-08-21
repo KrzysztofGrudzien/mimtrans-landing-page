0986b3f50b951287580cc1670caa05647db53535
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2021-08-21 19:43:20 +0200

    change styles for rest files

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import Contact from './components/Contact';
 import CallToAction from './components/CallToAction';
+import Testimonials from './components/Testimonials';
 
 const App = () => {
     return (
         <>
             <Header />
+            <Testimonials />
             <CallToAction />
             <Contact />
             <Footer />
