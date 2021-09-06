021a6ca5c26af4b1263513517ad68c746095dbcf
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2021-09-06 13:33:25 +0200

    create a global context for App component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
+import { useState } from 'react';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Main from './components/Main';
+import { AppContext, defaultOptions } from './context/appContext';
 
 const App = () => {
+    const [isOpenTimetable, setIsOpenTimetable] = useState(defaultOptions.isOpenTimetable);
+    const [isOpenNews, setIsOpenNews] = useState(defaultOptions.isOpenNews);
+    const [isOpenMenu, setIsOpenMenu] = useState(defaultOptions.isOpenMenu);
+
+    const handleIsOpenTimetable = () => {
+        setIsOpenTimetable(!isOpenTimetable);
+    };
+
+    const handleIsOpenNews = () => {
+        setIsOpenNews(!isOpenNews);
+    };
+
+    const handleIsOpenMenu = () => {
+        setIsOpenMenu(!isOpenMenu);
+    };
+
     return (
         <>
-            <Header />
-            <Main />
-            <Footer />
+            <AppContext.Provider
+                value={{
+                    isOpenTimetable,
+                    isOpenNews,
+                    isOpenMenu,
+                    toggleTimetableState: handleIsOpenTimetable,
+                    toggleNewsState: handleIsOpenNews,
+                    toggleMenuState: handleIsOpenMenu,
+                }}
+            >
+                <Header />
+                <Main />
+                <Footer />
+            </AppContext.Provider>
         </>
     );
 };
