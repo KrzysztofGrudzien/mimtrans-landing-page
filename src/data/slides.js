4c1e47c3e521b55d480aee54959214526a1f6fc7
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2021-09-08 16:04:24 +0200

    create data for testimonials

---
+++ src/data/slides.js
@@ -0,0 +1,40 @@
+import imageOne from '../assets/img/referencja-1.png';
+
+export const slides = [
+    {
+        title: 'Dom Pracy Twórczej w Radziejowice',
+        description:
+            'Proszę przyjąć moje serdeczne podziękowania za dotychczasową współpracę. Chciałbym pogratulować Panu świadczenia usług transportowych na najwyższym poziomie. Pana oraz Pańscy Współpracownicy dali się poznać jako zgrany i profesjonalny zespół. Pana zaangażowanie, życzliwość, elastyczność, sprawiają, że zdobywamy uznanie w oczach naszych gości. To również dzięki Pana firmie Pałac w Radziejowicach postrzegany jest jako miejsce z obsługą najwyższej jakości. Licząc na kolejne lata tak owocnej współpracy, przesyłam wyrazy szacunku.',
+        image: imageOne,
+        alt: 'miniaturka zdjęcia referencji z domu pracy twórczejw w Radziejowicach',
+        name: 'Bogumił Mrówczyński - dyrektor',
+        subtitle: '"Dom Pracy Twórczej w Radziejowice"',
+    },
+    {
+        title: 'Stowarzyszenie "SZANSA" w Żabiej Woli',
+        description:
+            'Zarząd Stowarzyszenia "SZANSA" w Żabiej Woli gorąco i serdecznie dziękuję Panu Jerzemu Wiśniewskiemu. W imieniu naszego Stowarzyszenia pragniemy warazić głęboką wdzięczność za ofiarowaną pomoc i życzliwość',
+        image: imageOne,
+        image: imageOne,
+        alt: 'miniaturka zdjęcia referencji ze Stowarzyszenie "SZANSA" w Żabiej Woli',
+        name: 'Małgorzata Kozicka - prezes stowarzyszenia',
+        subtitle: 'Stowarzyszenie "SZANSA" w Żabiej Woli',
+    },
+    {
+        title: 'Szkoła Podstawowa im. Kawalerów Orderu Uśmiechu w Józefinie',
+        description:
+            'Dyrektor, Grono Pedagogiczne oraz Uczniowie Szkoła Podstawowa im. Kawalerów Orderu Uśmiechu w Józefinie serdecznie dziękują panu Jerzemu Wiśniewskiemu',
+        image: imageOne,
+        alt: 'miniaturka zdjęcia referencji ze Szkoły Podstawowej im. Kawalerów Orderu Uśmiechu w Józefinie',
+        name: 'Marzena Pytlak - dyrektor szkoły',
+        subtitle: '"Szkoła Podstawowa im. Kawalerów Orderu Uśmiechu w Józefinie"',
+    },
+    {
+        title: 'Zespół FRESH z UKS "Olimpijczyk 2008',
+        description: 'Dziękuje przyjaciołom zespołu za wsparcie w wyjeździe na XVI MPCh 2013',
+        image: imageOne,
+        alt: 'miniaturka zdjęcia referencji Zespołu FRESH z UKS "Olimpijczyk',
+        name: 'Zespół FRESH',
+        subtitle: '"Zespół FRESH z UKS "Olimpijczyk 2008"',
+    },
+];
